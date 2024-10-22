bf79493e3f40c1b669bc8f1e4599926074c9e926
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-10-22 15:16:36 +0600

    Add files via upload

---
+++ CODE/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+string usrName;
+ Console.WriteLine("Enter your username: ");
+
+ usrName =Console.ReadLine();
+
+Console.WriteLine("Username is: " + usrName);
+
+Console.WriteLine("Enter your age:");
+
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("your age is: " + age);
+
+
+const double pi = 3.14;
+
+double r;
+Console.WriteLine("Enter Radius: ");
+r = Convert.ToDouble(Console.ReadLine());
+double areaCircle = pi * r * r;
+
+Console.WriteLine("Radius:{0} ,Area: {1}",r, areaCircle);
+
+
+  
