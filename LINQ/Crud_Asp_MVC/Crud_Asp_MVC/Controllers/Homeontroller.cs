7d762dd8959834d398137f8cf95c7925fdf42064
Sawon <141425167+SawonAnis@users.noreply.github.com>
2025-01-03 22:51:54 +0600

    LINQ to SQL

---
+++ LINQ/Crud_Asp_MVC/Crud_Asp_MVC/Controllers/Homeontroller.cs
@@ -0,0 +1,21 @@
+﻿
+using Crud_Asp_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud_Asp_MVC.Controllers
+{
+    public class Homeontroller : Controller
+    {
+
+        StudentDBDataContext db = new StudentDBDataContext();
+        public ActionResult Index()
+        {
+            var data = db.students.ToList();    
+            return View(data);
+        }
+    }
+}
