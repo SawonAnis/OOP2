5d186bd1d3e227749a90b2389ee3e9b97a078b9c
Sawon <141425167+SawonAnis@users.noreply.github.com>
2025-04-30 19:52:39 +0600

    try.....catch excep.

---
+++ CODE/Exception_1.cs
@@ -0,0 +1,28 @@
+using System;
+namespace ExceptionHandlingDemo
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      int num1, num2, result;
+
+      try
+      {
+        Console.WriteLine("Enter First Number:");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Second Number:");
+        num2 = int.Parse(Console.ReadLine());
+
+        result = num1 / num2;
+        Console.WriteLine("Result: " + result);
+      }
+
+      catch
+      {
+        Console.WriteLine("Some Error Occurred.......");
+      }
+      
+    }
+  }
+}
