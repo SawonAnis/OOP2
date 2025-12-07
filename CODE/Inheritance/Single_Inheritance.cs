50f155bcf98a17a19549b4ef91d20bcd46f5964e
Sawon <141425167+SawonAnis@users.noreply.github.com>
2025-12-07 16:39:35 +0600

    Single Inheritance

---
+++ CODE/Inheritance/Single_Inheritance.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace InheritanceDemo
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic animal sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+  
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+        Animal myDog = new Dog();
+        myDog.MakeSound(); 
+    }
+  }
+}
